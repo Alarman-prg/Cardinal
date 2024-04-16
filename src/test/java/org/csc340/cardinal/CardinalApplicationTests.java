b5bd261770601395f73b695fa3841be48e781114
Alarman-prg <jepgpersonal@gmail.com>
2024-04-16 16:08:49 -0400

    init

---
+++ src/test/java/org/csc340/cardinal/CardinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csc340.cardinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
