a72a470087aa78c973b3c7aab9464ffe0cc35427
Alarman-prg <jepgpersonal@gmail.com>
2024-04-22 22:00:54 -0400

    county html changes

--- src/main/java/com/csc/cardinal/park/ParkController.java
+++ src/main/java/com/csc/cardinal/park/ParkController.java
@@ -1,18 +1,32 @@
 package com.csc.cardinal.park;
 
 
+import com.csc.cardinal.user.GroupHikeEntity;
+import com.csc.cardinal.user.GroupHikeRepository;
 import com.csc.cardinal.user.UserEntity;
 import com.csc.cardinal.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ParkController {
 
     @Autowired
     private ParkService parkService;
+
+    @Autowired
+    private GroupHikeRepository groupHikeRepository;
+
+    @GetMapping("/county")
+    public String displayCounty(Model model) {
+        List<GroupHikeEntity> hikeList = groupHikeRepository.findAll();
+        model.addAttribute("hikeList", hikeList);
+        return "park/county";
+    }
 
     @PostMapping("/parks")
     public ParkEntity save(@RequestBody ParkEntity parkEntity) {
