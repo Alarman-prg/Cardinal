6cb435c8ce4b47e1b6a8a9960c6a9e3564707a8a
Alarman-prg <jepgpersonal@gmail.com>
2024-04-29 23:23:09 -0400

    default users in schema

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS 'users' (
-    'id' int AUTO_INCREMENT PRIMARY KEY,
-    'username' VARCHAR(255) NOT NULL,
-    'email' VARCHAR(255) NOT NULL,
-    'password' VARCHAR(255) NOT NULL,
-    'firstname' VARCHAR(255) NOT NULL,
-    'lastname' VARCHAR(255) NOT NULL,
-);
